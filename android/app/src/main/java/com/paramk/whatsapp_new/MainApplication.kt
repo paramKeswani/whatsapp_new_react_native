7449ba80a491ea8c32791a864f1fa5c0fca56e84
paramKeswani <paramkeswani19@gmail.com>
2024-05-14 11:08:02 +0530

    updating the thing

--- android/app/src/main/java/com/paramk/whatsapp_new/MainApplication.kt
+++ android/app/src/main/java/com/paramk/whatsapp_new/MainApplication.kt
@@ -1,60 +1,49 @@
 package com.paramk.whatsapp_new
-import io.invertase.firebase.app.ReactNativeFirebaseAppModule
 
 import android.app.Application
 import android.content.res.Configuration
-
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.ReactHost
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
-
 import expo.modules.ApplicationLifecycleDispatcher
 import expo.modules.ReactNativeHostWrapper
-import io.invertase.firebase.app.ReactNativeFirebaseAppPackage
 
 class MainApplication : Application(), ReactApplication {
-
-  override val reactNativeHost: ReactNativeHost = ReactNativeHostWrapper(
+    private val mReactNativeHost: ReactNativeHost = ReactNativeHostWrapper(
         this,
         object : DefaultReactNativeHost(this) {
-          override fun getPackages(): List<ReactPackage> {
-                      val packages = mutableListOf<ReactPackage>()
-                // Include the existing packages
-                packages.addAll(PackageList(this).packages)
-                // Add the following line to include ReactNativeFirebaseAppPackage
-                packages.add(io.invertase.firebase.app.ReactNativeFirebaseAppModule())
+            override fun getPackages(): List<ReactPackage> {
+                val packages: MutableList<ReactPackage> = PackageList(this).packages.toMutableList()
+                // Add additional packages here
+                // packages.add(ReactNativeFirebaseAppPackage()) // Remove this line
                 return packages
-          }
+            }
 
-          override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
+            override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
+            override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+            override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+            override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+        }
+    )
 
-          override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+    override val reactNativeHost: ReactNativeHost
+        get() = mReactNativeHost
 
-          override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-          override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
-      }
-  )
-
-  override val reactHost: ReactHost
-    get() = ReactNativeHostWrapper.createReactHost(applicationContext, reactNativeHost)
-
-  override fun onCreate() {
-    super.onCreate()
-    SoLoader.init(this, false)
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
-      load()
+    override fun onCreate() {
+        super.onCreate()
+        SoLoader.init(this, false)
+        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+            load()
+        }
+        ApplicationLifecycleDispatcher.onApplicationCreate(this)
     }
-    ApplicationLifecycleDispatcher.onApplicationCreate(this)
-  }
 
-  override fun onConfigurationChanged(newConfig: Configuration) {
-    super.onConfigurationChanged(newConfig)
-    ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig)
-  }
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig)
+    }
 }
